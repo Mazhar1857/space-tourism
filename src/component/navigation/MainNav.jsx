d60a83768c93d88c7ec73df184badd2d004b2394
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-04-23 14:24:36 +0530

    initial commit

---
+++ src/component/navigation/MainNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './mainNav.css';
+
+const MainNav = () => {
+    return (
+        <div className='main-navigation'>
+           
+        </div>
+    )
+}
+
+export default MainNav
---
+++ src/component/navigation/TechnologyNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TechnologyNav = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TechnologyNav
