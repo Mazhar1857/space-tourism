b969bb8cb4c781ffd61dc53eb1a22cfbe6157fec
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-04-25 04:43:28 +0530

    add destination navigation

--- src/component/destination/Destination.jsx
+++ src/component/destination/Destination.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import DestinationNav from '../navigation/DestinationNav';
+import data from '../../data.json';
 
 const Destination = () => {
     return (
-        <div>
+        <div className='destination-page'>
+            <h1 className='destination-heading'>Pick your destination</h1>
+            <div className='destinaiton-navigation'><DestinationNav /></div>
+            <figure className='globe'>
+                <img src={data["destinations"][0]["images"]["png"]} alt="this is moon" />
+            </figure>
+            <div className='planet'>
+                <h1>Moon</h1>
+                <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2  and Apollo 11 landing sites.</p>
+                <div className='distance-time'>
+                    <div>
+                        <div>AVG. DISTANCE</div>
+                        <div>384,400 KM</div>
+                    </div>
+                    <div>
+                        <div>Est. travel time</div>
+                        <div>3 days</div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
