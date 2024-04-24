65c84070b07bcbbb37f55762b0fb864661bac07b
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-04-24 10:47:56 +0530

    add mainNav bar

--- src/component/navigation/MainNav.jsx
+++ src/component/navigation/MainNav.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './mainNav.css';
 
 const MainNav = () => {
+
+    const [mobileNav, setMobileNav] = useState(false);
+    const [IsActive, setActive] = useState(0);
+
+    const handleActiveNav = (n) => {
+        setActive(n);
+    }
+
+    const toggleMobileNav = () => {
+        setMobileNav((pre) => {
+            return !pre;
+        })
+    }
+
     return (
         <div className='main-navigation'>
-           
+            <div className='main-nav-logo'>
+                <a href=""> <img src="/assets/shared/logo.svg" alt="" /></a>
+                <div className='main-nav-horizontal-line'></div>
+            </div>
+            <button className='mobile-nav-button' onClick={toggleMobileNav}><img src={!mobileNav ? '/assets/shared/icon-hamburger.svg' : '/assets/shared/icon-close.svg'} alt="" /></button>
+            <ul role='list' className={`main-nav-list ${mobileNav ? 'mobile-nav' : ''}`}>
+                <li className={IsActive === 1 ? 'active' : ''} onClick={() => handleActiveNav(1)}><a>HOME</a></li>
+                <li className={IsActive === 2 ? 'active' : ''} onClick={() => handleActiveNav(2)}><a>DESTINATION</a></li>
+                <li className={IsActive === 3 ? 'active' : ''} onClick={() => handleActiveNav(3)}><a >CREW</a></li>
+                <li className={IsActive === 4 ? 'active' : ''} onClick={() => handleActiveNav(4)}><a >TECHNOLOGY</a></li>
+            </ul>
         </div>
     )
 }
