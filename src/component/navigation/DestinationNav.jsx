1ac16b7da8e6375f3ecd44cdc189bfc6258ac3e5
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-04-25 13:25:31 +0530

    add dynamic state in nav

--- src/component/navigation/DestinationNav.jsx
+++ src/component/navigation/DestinationNav.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './destinationNav.css';
 
-const DestinationNav = () => {
+const DestinationNav = ({ toggleGlobe }) => {
+
+    const [isActive, setIsActive] = useState(1);
+
+    const toggleDestinationNav = (n) => {
+        toggleGlobe(n);
+        setIsActive(n);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsActive((pre) => {
+                return (pre % 4) + 1;
+            })
+            toggleGlobe(((isActive) % 4) + 1)
+
+        }, 3000);
+
+        return () => {
+            clearInterval(interval);
+        }
+
+    })
+
     return (
-        <div>
-
+        <div className='destination-nav'>
+            <div className={isActive === 1 ? 'active' : ''} onClick={() => toggleDestinationNav(1)}>MOON</div>
+            <div className={isActive === 2 ? 'active' : ''} onClick={() => toggleDestinationNav(2)}>MARS</div>
+            <div className={isActive === 3 ? 'active' : ''} onClick={() => toggleDestinationNav(3)}>EUROPA</div>
+            <div className={isActive === 4 ? 'active' : ''} onClick={() => toggleDestinationNav(4)}>TITAN</div>
         </div>
     )
 }
