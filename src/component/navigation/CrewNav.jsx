de5ad6b2d53f5a74e7006bb844d959b0730ce632
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-04-25 18:26:51 +0530

    add navigation component for crew

--- src/component/navigation/CrewNav.jsx
+++ src/component/navigation/CrewNav.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './crewNav.css';
 
-const CrewNav = () => {
+const CrewNav = ({ toggleCrew }) => {
+    const [isActive, setIsActive] = useState(3);
+
+    const toggleCrewMember = (n) => {
+        toggleCrew(n);
+        setIsActive(n);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsActive((pre) => {
+                return (pre % 4) + 1;
+            })
+            toggleCrew(((isActive) % 4) + 1)
+
+        }, 3000);
+
+        return () => {
+            clearInterval(interval);
+        }
+
+    })
+
     return (
-        <div>
-
+        <div className='crew-nav'>
+            <div className={isActive === 1 ? 'active' : ''} onClick={() => toggleCrewMember(1)}></div>
+            <div className={isActive === 2 ? 'active' : ''} onClick={() => toggleCrewMember(2)}></div>
+            <div className={isActive === 3 ? 'active' : ''} onClick={() => toggleCrewMember(3)}></div>
+            <div className={isActive === 4 ? 'active' : ''} onClick={() => toggleCrewMember(4)}></div>
         </div>
     )
+
 }
 
 export default CrewNav
