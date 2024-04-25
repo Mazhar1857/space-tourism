e631ad2adbd8f6d407779c468dfb753d9ec5b8fe
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-04-25 18:24:55 +0530

    add landing page

--- src/component/landingPage/LandingPage.jsx
+++ src/component/landingPage/LandingPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './landingPage.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className='landing-page'>
+            <div className='description'>
+                <h5>SO, YOU WANT TO TRAVEL TO</h5>
+                <h1>SPACE</h1>
+                <p>Let's face it; if you want to go space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+            </div>
+            <div className='explore'>
+                EXPLORE
+            </div>
 
         </div>
     )
