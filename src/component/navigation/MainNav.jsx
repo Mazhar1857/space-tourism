441e302926656e093a3ac2db1fffdd0d291fdc65
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-04-25 19:09:14 +0530

    fixed logo and mobile nav button

--- src/component/navigation/MainNav.jsx
+++ src/component/navigation/MainNav.jsx
@@ -29,10 +29,10 @@ const MainNav = ({ toggleNav }) => {
     return (
         <div className='main-navigation'>
             <div className='main-nav-logo'>
-                <a href=""> <img src="/assets/shared/logo.svg" alt="" /></a>
+                <a href=""> <img src="assets/shared/logo.svg" alt="" /></a>
                 <div className='main-nav-horizontal-line'></div>
             </div>
-            <button className='mobile-nav-button' onClick={toggleMobileNav}><img src={!mobileNav ? '/assets/shared/icon-hamburger.svg' : '/assets/shared/icon-close.svg'} alt="" /></button>
+            <button className='mobile-nav-button' onClick={toggleMobileNav}><img src={!mobileNav ? 'assets/shared/icon-hamburger.svg' : 'assets/shared/icon-close.svg'} alt="" /></button>
             <ul role='list' className={`main-nav-list ${mobileNav ? 'mobile-nav' : ''}`}>
                 <li className={IsActive === 1 ? 'active' : ''} onClick={() => handleActiveNav(1)}><a>HOME</a></li>
                 <li className={IsActive === 2 ? 'active' : ''} onClick={() => handleActiveNav(2)}><a>DESTINATION</a></li>
