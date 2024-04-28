85d2d21adcdbd3a4cb13434b31bc6789d8f646dd
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-04-28 12:29:05 +0530

    add top margin and flex

--- src/component/landingPage/LandingPage.jsx
+++ src/component/landingPage/LandingPage.jsx
@@ -3,16 +3,17 @@ import './landingPage.css';
 
 const Home = () => {
     return (
-        <div className='landing-page'>
-            <div className='description'>
-                <h5>SO, YOU WANT TO TRAVEL TO</h5>
-                <h1>SPACE</h1>
-                <p>Let's face it; if you want to go space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+        <div className='landing-page-parent'>
+            <div className='landing-page'>
+                <div className='description'>
+                    <h5>SO, YOU WANT TO TRAVEL TO</h5>
+                    <h1>SPACE</h1>
+                    <p>Let's face it; if you want to go space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+                </div>
+                <div className='explore'>
+                    EXPLORE
+                </div>
             </div>
-            <div className='explore'>
-                EXPLORE
-            </div>
-
         </div>
     )
 }
